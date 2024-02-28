d9b88f6cdd41e8b42893a78ead169a426e6bf01b
aulafaridatuz <aulafaridatuz@gmail.com>
2024-02-28 14:42:22 +0700

    setup login

--- internals/handlers/auth.handler.go
+++ internals/handlers/auth.handler.go
@@ -3,7 +3,8 @@ package handlers
 import (
 	"bdpit/bookstore-go/internals/models"
 	"bdpit/bookstore-go/internals/repositories"
-	_"hash"
+	"bdpit/bookstore-go/pkg"
+	_ "hash"
 	"log"
 	"net/http"
 
@@ -42,12 +43,12 @@ func (a *AuthHandler) Register(ctx *gin.Context) {
 	// checking duplicate email
 	if len(result) > 0 {
 		ctx.JSON(http.StatusBadRequest, gin.H{
-			"message" : "Email is already regitered!",
+			"message": "Email is already regitered!",
 		})
 		return
 	}
 
-	hash,err :=	argon2id.CreateHash(body.Password, argon2id.DefaultParams)
+	hash, err := argon2id.CreateHash(body.Password, argon2id.DefaultParams)
 	if err != nil {
 		log.Println(err.Error())
 		ctx.JSON(http.StatusInternalServerError, gin.H{
@@ -57,7 +58,7 @@ func (a *AuthHandler) Register(ctx *gin.Context) {
 	}
 
 	if err := a.SaveUser(models.AuthModel{
-		Email: body.Email,
+		Email:    body.Email,
 		Password: hash,
 	}); err != nil {
 		log.Println(err.Error())
@@ -68,10 +69,63 @@ func (a *AuthHandler) Register(ctx *gin.Context) {
 	}
 
 	ctx.JSON(http.StatusCreated, gin.H{
-		"message":"Success Registered!",
+		"message": "Success Registered!",
 	})
 }
 
 func (a *AuthHandler) Login(ctx *gin.Context) {
+	body := models.AuthModel{}
+	if err := ctx.ShouldBind(&body); err != nil {
+		log.Println(err.Error())
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
 
+	result, err := a.FindByEmail(body)
+	if err != nil {
+		log.Println(err.Error())
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	if len(result) == 0 {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"message": "Email isn't registered!",
+		})
+		return
+	}
+
+	match, err := argon2id.ComparePasswordAndHash(body.Password, result[0].Password)
+	if err != nil {
+		log.Println(err.Error())
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	if !match {
+		ctx.JSON(http.StatusUnauthorized, gin.H{
+			"message": "Bad Credentials!",
+		})
+		return
+	}
+
+	payload := pkg.NewPayload(body.Email)
+	token, err := payload.CreateToken()
+	if err != nil {
+		log.Println(err.Error())
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"message":"Login Success!",
+		"token": token,
+	})
 }
